760f4ed67b4c3f8167ff99b3de7a307e91ee97e8
mengdangdang <793490587@qq.com>
2022-09-20 22:24:20 +0800

    ddd

--- 02代码校验/Entrance.py
+++ 02代码校验/Entrance.py
@@ -3,5 +3,15 @@
 # @Author  : 孟砀砀
 # @File    : Entrance.py
 # @Software: PyCharm
+from Deal_Words import Deal_Words
 
+class Entrance:
+    def __init__(self):
+        pass
 
+    def Entrance(self,str_world):
+        pass
+
+# Deal_Words
+if __name__ == '__main__':
+    print(Deal_Words().Deal_DML_Words(""))
---
+++ 02代码校验/test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/20 21:54
+# @Author  : 孟砀砀
+# @File    : test.py
+# @Software: PyCharm
+# with open('test.txt', 'r', encoding='utf-8') as
